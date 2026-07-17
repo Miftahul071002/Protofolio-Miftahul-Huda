329d2501d0bccbe2d320e553c34edc94ccd377be
Miftahul Huda <122281491+Miftahul071002@users.noreply.github.com>
2026-07-17 17:33:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,4 +24,19 @@ window.addEventListener('scroll', () => {
   } else {
     navbar.style.background = 'rgba(15,23,42,0.8)';
   }
+
+  // Toggle hamburger menu
+const toggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+toggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+// Tutup menu setelah klik link
+document.querySelectorAll('#nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+  });
+});
 });
